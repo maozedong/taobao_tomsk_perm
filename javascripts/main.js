cfbc0f79bda9256fc027ffae3ed2ebdfb1834a3e
maozedong <verh@yandex.ru>
2013-03-04 17:29:05 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,39 @@
 console.log('This would be the main JS file.');
 var num_regex = /^[-+]?[0-9]*\.?[0-9]+$/; // numeric digits only
 $(document).ready(function(){
+
+$('.button.men').click(function(){
+	$('table.men').show();
+	$('table.women').hide();
+	$('table.children').hide();
+	$('table.other').hide();
+	$('.button').removeClass('active');
+	$(this).addClass('active');
+});
+$('.button.women').click(function(){
+	$('table.men').hide();
+	$('table.women').show();
+	$('table.children').hide();
+	$('table.other').hide();
+	$('.button').removeClass('active');
+	$(this).addClass('active');
+});
+$('.button.children').click(function(){
+	$('table.men').hide();
+	$('table.women').hide();
+	$('table.children').show();
+	$('table.other').hide();
+	$('.button').removeClass('active');
+	$(this).addClass('active');
+});
+$('.button.other').click(function(){
+	$('table.men').hide();
+	$('table.women').hide();
+	$('table.children').hide();
+	$('table.other').show();
+	$('.button').removeClass('active');
+	$(this).addClass('active');
+});
 	$('.super-wrapper input').val('');
   var cTao = {
 	price : 0,
@@ -8,8 +41,13 @@ $(document).ready(function(){
 	weight : 0,
 	total : 0,
 	calc:function(){
+		if(this.price != 0 && this.quantity != 0 && this.weight !=0){
+		this.total = 0;
 		this.total = ((this.price*this.quantity)*1.25+(this.price*this.quantity*0.1))*5.5 + (this.quantity*this.weight*450) + 106;
 		this.total = this.total.toFixed(2);
+		}else{
+			this.total = 0;
+		}
 	}
   }
   var cAli = {
@@ -17,18 +55,36 @@ $(document).ready(function(){
 	quantity : 0,
 	total : 0,
 	calc:function(){
+		if(this.price != 0 && this.quantity != 0){
+		this.total = 0;
 		this.total = this.price*this.quantity*1.4*32;
 		this.total = this.total.toFixed(2);
+		}else{
+			this.total = 0;
+		}
 	}
   }
   var cVS = {
 	price : 0,
 	quantity : 0,
 	total : 0,
-	delivary: 0,
+	condition : 0,
 	calc:function(){
-		this.total = (this.price*this.quantity)*31*1.25+1488;
+		if(this.price != 0 && this.quantity != 0){
+		this.total = 0;
+		this.condition = 0;
+		if(this.price > 0 && this.price <= 75 ){
+			this.condition = 868;
+		}else if(this.price > 75 && this.price <= 125 ){
+			this.condition = 1054;
+		}else if(this.price > 125 && this.price < 175 ){
+			this.condition = 1209;
+		}
+		this.total = (this.price*this.quantity)*31*1.25 + this.condition;
 		this.total = this.total.toFixed(2);
+		}else{
+			this.total = 0;
+		}
 	}
   }
   var cTur = {
@@ -36,19 +92,25 @@ $(document).ready(function(){
 	quantity : 0,
 	weight : 0,
 	total : 0,
+	condition :0,
 	calc:function(){
+		if(this.price != 0 && this.quantity != 0 && this.weight != 0){
 		this.total = 0;
-		if(this.price < 100){
-			this.price = 10;
-		}else{
-			this.price = this.price*0.1;
+		this.condition = 0;
+		if(this.price <= 100 && this.price > 0 ){
+			this.condition = 10;
+		}else if(this.price > 100){
+			this.condition = this.price*0.1;
 		}
-		this.total = this.price*this.quantity*1.25*18.4 + this.weight*355;
+		this.total = this.price*this.quantity*1.25*18.4 + this.weight*355 + this.condition;
 		this.total = this.total.toFixed(2);
+		}else{
+			this.total = 0;
+		}
 	}
   }
   /////cTao/////////////////////
-  $('.calcTao .price').change(function(){
+  $('.calcTao .price').keyup(function(){
   if( $.trim($(this).val()).match(num_regex)){
 	cTao.price = $('.calcTao .price').val();
 	cTao.quantity = $('.calcTao .quantity').val();
@@ -60,19 +122,19 @@ $(document).ready(function(){
 	cTao.calc();
 	$('.calcTao .total').val(cTao.total.toString());
   });
-  $('.calcTao .quantity').change(function(){
+  $('.calcTao .quantity').keyup(function(){
   if( $.trim($(this).val()).match(num_regex)){
 	cTao.price = $('.calcTao .price').val();
 	cTao.quantity = $('.calcTao .quantity').val();
 	cTao.weight = $('.calcTao .weight').val();
 	}else{
 		$(this).val('');
-	cTao.quantity = $('.calcTao .quantity').val();
+		cTao.quantity = $('.calcTao .quantity').val();
 	}
 	cTao.calc();
 	$('.calcTao .total').val(cTao.total.toString());
   });
-    $('.calcTao .weight').change(function(){
+    $('.calcTao .weight').keyup(function(){
 	if( $.trim($(this).val()).match(num_regex)){
 	cTao.price = $('.calcTao .price').val();
 	cTao.quantity = $('.calcTao .quantity').val();
@@ -87,7 +149,7 @@ $(document).ready(function(){
   ///////////cTao//////////////////
   
   /////cAli/////////////////////
-  $('.calcAli .price').change(function(){
+  $('.calcAli .price').keyup(function(){
 	if( $.trim($(this).val()).match(num_regex)){
 	cAli.price = $('.calcAli .price').val();
 	cAli.quantity = $('.calcAli .quantity').val();
@@ -99,7 +161,7 @@ $(document).ready(function(){
 	cAli.calc();
 	$('.calcAli .total').val(cAli.total.toString());
   });
-  $('.calcAli .quantity').change(function(){
+  $('.calcAli .quantity').keyup(function(){
   if( $.trim($(this).val()).match(num_regex)){
 	cAli.price = $('.calcAli .price').val();
 	cAli.quantity = $('.calcAli .quantity').val();
@@ -111,7 +173,7 @@ $(document).ready(function(){
 	cAli.calc();
 	$('.calcAli .total').val(cAli.total.toString());
   });
-    $('.calcAli .weight').change(function(){
+    $('.calcAli .weight').keyup(function(){
 	if( $.trim($(this).val()).match(num_regex)){
 	cAli.price = $('.calcAli .price').val();
 	cAli.quantity = $('.calcAli .quantity').val();
@@ -126,7 +188,7 @@ $(document).ready(function(){
   ///////////cAli//////////////////
   
   /////cVS/////////////////////
-  $('.calcVS .price').change(function(){
+  $('.calcVS .price').keyup(function(){
   if( $.trim($(this).val()).match(num_regex)){
 	cVS.price = $('.calcVS .price').val();
 	cVS.quantity = $('.calcVS .quantity').val();
@@ -138,7 +200,7 @@ $(document).ready(function(){
 	cVS.calc();
 	$('.calcVS .total').val(cVS.total.toString());
   });
-  $('.calcVS .quantity').change(function(){
+  $('.calcVS .quantity').keyup(function(){
   if( $.trim($(this).val()).match(num_regex)){
 	cVS.price = $('.calcVS .price').val();
 	cVS.quantity = $('.calcVS .quantity').val();
@@ -150,7 +212,7 @@ $(document).ready(function(){
 	cVS.calc();
 	$('.calcVS .total').val(cVS.total.toString());
   });
-    $('.calcVS .weight').change(function(){
+    $('.calcVS .weight').keyup(function(){
 	if( $.trim($(this).val()).match(num_regex)){
 	cVS.price = $('.calcVS .price').val();
 	cVS.quantity = $('.calcVS .quantity').val();
@@ -165,7 +227,7 @@ $(document).ready(function(){
   ///////////cVS//////////////////
   
   /////cTur/////////////////////
-  $('.calcTur .price').change(function(){
+  $('.calcTur .price').keyup(function(){
   if( $.trim($(this).val()).match(num_regex)){
 	cTur.price = $('.calcTur .price').val();
 	cTur.quantity = $('.calcTur .quantity').val();
@@ -178,7 +240,7 @@ $(document).ready(function(){
 	$('.calcTur .total').val(cTur.total.toString());
   });
   
-  $('.calcTur .quantity').change(function(){
+  $('.calcTur .quantity').keyup(function(){
   if( $.trim($(this).val()).match(num_regex)){
 	cTur.price = $('.calcTur .price').val();
 	cTur.quantity = $('.calcTur .quantity').val();
@@ -188,13 +250,10 @@ $(document).ready(function(){
 	cTur.quantity = $('.calcTur .quantity').val();
 	}
 	cTur.calc();
-		if(cTur.total != 0){
-		$('.calcTur .total').val(cTur.total.toString());
-	}else{
-		$('.calcTur .total').val('');
-	}
+	$('.calcTur .total').val(cTur.total.toString());
   });
-    $('.calcTur .weight').change(function(){
+  
+    $('.calcTur .weight').keyup(function(){
 	if( $.trim($(this).val()).match(num_regex)){
 	cTur.price = $('.calcTur .price').val();
 	cTur.quantity = $('.calcTur .quantity').val();
@@ -204,11 +263,7 @@ $(document).ready(function(){
 		cTur.weight = $('.calcTur .weight').val();
 	}
 	cTur.calc();
-	if(cTur.total != 0){
-		$('.calcTur .total').val(cTur.total.toString());
-	}else{
-		$('.calcTur .total').val('');
-	}
+	$('.calcTur .total').val(cTur.total.toString());
   });
   ///////////cTur//////////////////
 });
